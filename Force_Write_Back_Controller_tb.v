0de05e7e9407df66b1e9714068f94e7fe419a971
Ethan <ethanyang4623@gmail.com>
2018-12-03 17:34:29 -0500

    simulation all pass

--- Force_Write_Back_Controller_tb.v
+++ Force_Write_Back_Controller_tb.v
@@ -61,6 +61,9 @@ module Force_Write_Back_Controller_tb;
 	
 	reg [CELL_ADDR_WIDTH-1:0] particle_address;
 	
+	reg [6:0] global_counter;				// range: 0~127
+	reg [4:0] tmp_counter;					// covering range 1~31
+	
 	always@(*)
 		begin
 		in_particle_id <= {4'd2,4'd2,4'd2, particle_address};
@@ -68,6 +71,135 @@ module Force_Write_Back_Controller_tb;
 		
 	always #1 clk <= ~clk;
 	
+	always@(posedge clk)
+		begin
+		if(rst)
+			begin
+			global_counter <= 1;
+			tmp_counter <= 1;
+			
+			in_partial_force_valid <= 1'b0;
+			particle_address <= 9'd0;
+			in_partial_force <= {32'h3F800000, 32'h3F800000, 32'h3F800000};				// input value: {1.0,1.0,1.0}
+			in_read_data_request <= 1'b0;
+			in_cache_read_address <= 9'd0;
+			end
+		else if(global_counter == 0)
+			begin
+			global_counter <= 1;
+			tmp_counter <= 1;
+			
+			in_partial_force_valid <= 1'b0;
+			particle_address <= 9'd0;
+			in_partial_force <= {32'h3F800000, 32'h3F800000, 32'h3F800000};				// input value: {1.0,1.0,1.0}
+			in_read_data_request <= 1'b0;
+			in_cache_read_address <= 9'd0;
+			end
+		// Acc 1 to particle 1~20
+		else if(global_counter < 20)
+			begin
+			global_counter <= global_counter + 1'b1;
+			tmp_counter <= 1;
+			
+			in_partial_force_valid <= 1'b1;
+			particle_address <= {2'd0, global_counter};
+			in_partial_force <= {32'h3F800000, 32'h3F800000, 32'h3F800000};				// input value: {1.0,1.0,1.0}
+			in_read_data_request <= 1'b0;
+			in_cache_read_address <= 9'd0;
+			end
+		// Acc fix number to address 1~3
+		else if(global_counter < 40)
+			begin
+			// Disable particle read
+			in_read_data_request <= 1'b0;
+			in_cache_read_address <= 9'd0;
+			// Assign global_counter
+			global_counter <= global_counter + 1'b1;
+			// Assign tmp_counter
+			if(tmp_counter == 3)
+				tmp_counter <= 1;
+			else
+				tmp_counter <= tmp_counter + 1'b1;
+
+			// Assign force input
+			if(tmp_counter == 1)
+				begin
+				in_partial_force_valid <= 1'b1;
+				particle_address <= {4'd0, tmp_counter};
+				in_partial_force <= {32'h40080000, 32'h40080000, 32'h40080000};				// input value: {2.125,2.125,2.125}
+				end
+			else if(tmp_counter == 2)
+				begin
+				in_partial_force_valid <= 1'b1;
+				particle_address <= {4'd0, tmp_counter};
+				in_partial_force <= {32'h417E0000, 32'h417E0000, 32'h417E0000};				// input value: {15.875,15.875,15.875}
+				end
+			else if(tmp_counter == 3)
+				begin
+				in_partial_force_valid <= 1'b1;
+				particle_address <= {4'd0, tmp_counter};
+				in_partial_force <= {32'h42E04000, 32'h42E04000, 32'h42E04000};				// input value: {112.125,112.125,112.125}
+				end
+			else
+				begin
+				in_partial_force_valid <= 1'b0;
+				particle_address <= 0;
+				in_partial_force <= {32'h3F800000, 32'h3F800000, 32'h3F800000};				// input value: {1.0,1.0,1.0}
+				end
+			end
+		// Disable input, wait for data process done
+		else if(global_counter < 90)
+			begin
+			// Assign global_counter
+			global_counter <= global_counter + 1'b1;
+			tmp_counter <= 1;
+			// Disable particle read
+			in_read_data_request <= 1'b0;
+			in_cache_read_address <= 9'd0;
+			// Invalidate input
+			in_partial_force_valid <= 1'b0;
+			particle_address <= 9'd0;
+			in_partial_force <= {32'h3F800000, 32'h3F800000, 32'h3F800000};				// input value: {1.0,1.0,1.0}
+			end
+		// Enable read
+		else if(global_counter < 125)
+			begin
+			// Assign global_counter
+			global_counter <= global_counter + 1'b1;
+			tmp_counter <= tmp_counter + 1'b1;
+			// Enable particle read
+			in_read_data_request <= 1'b1;
+			in_cache_read_address <= {4'd0, tmp_counter};
+			// Invalidate input
+			in_partial_force_valid <= 1'b0;
+			particle_address <= 9'd0;
+			in_partial_force <= {32'h3F800000, 32'h3F800000, 32'h3F800000};				// input value: {1.0,1.0,1.0}
+			end
+		// Wait for read finish
+		else
+			begin
+			// Assign global_counter
+			global_counter <= global_counter + 1'b1;
+			tmp_counter <= 1;
+			// Disable particle read
+			in_read_data_request <= 1'b0;
+			in_cache_read_address <= 9'd0;
+			// Invalidate input
+			in_partial_force_valid <= 1'b0;
+			particle_address <= 9'd0;
+			in_partial_force <= {32'h3F800000, 32'h3F800000, 32'h3F800000};				// input value: {1.0,1.0,1.0}
+			end
+		end
+		
+	//////////////////////////////////////////////////////////
+	// Final value:
+	//	1: 1.0+2.125*7 = 15.875 (32'h417E0000)
+	//	2: 1.0+15.875*7 = 112.125 (32'h42E04000)
+	//	3: 1.0+112.125*6 = 673.75 (32'h44287000)
+	//	4~19: 1.0 (32'h3F800000)
+	//	19~...: 0.0 (32'h00000000)
+	//  
+	//////////////////////////////////////////////////////////
 	
 	initial begin
 		clk <= 1'b1;
@@ -81,7 +213,7 @@ module Force_Write_Back_Controller_tb;
 		// Clear reset signal
 		#10
 		rst <= 1'b0;
-		
+/*		
 		// ID: 1, Value 1.0, for 5 cycles
 		#10
 		in_partial_force_valid <= 1'b1;
@@ -190,6 +322,76 @@ module Force_Write_Back_Controller_tb;
 		//	5: 1.0 (32'h3F800000)
 		//////////////////////////////////////////////////////////
 		
+		
+		// ID: 3, Value 1.36
+		#20
+		in_partial_force_valid <= 1'b1;
+		particle_address <= 9'd3;
+		in_partial_force <= {32'h3FAE47B0, 32'h3FAE47B0, 32'h3FAE47B0};				// input value: {1.36,1.36,1.36}
+		
+		// ID: 4, Value 2.48
+		#2
+		in_partial_force_valid <= 1'b1;
+		particle_address <= 9'd4;
+		in_partial_force <= {32'h401EB852, 32'h401EB852, 32'h401EB852};				// input value: {2.48,2.48,2.48}
+		
+		// ID: 5, Value 3.14
+		#2
+		in_partial_force_valid <= 1'b1;
+		particle_address <= 9'd5;
+		in_partial_force <= {32'h4048F5C3, 32'h4048F5C3, 32'h4048F5C3};				// input value: {3.14,3.14,3.14}
+		
+		// ID: 3, Value 1.36
+		#2
+		in_partial_force_valid <= 1'b1;
+		particle_address <= 9'd3;
+		in_partial_force <= {32'h3FAE47B0, 32'h3FAE47B0, 32'h3FAE47B0};				// input value: {1.36,1.36,1.36}
+		
+		// ID: 4, Value 2.48
+		#2
+		in_partial_force_valid <= 1'b1;
+		particle_address <= 9'd4;
+		in_partial_force <= {32'h401EB852, 32'h401EB852, 32'h401EB852};				// input value: {2.48,2.48,2.48}
+		
+		// ID: 5, Value 3.14
+		#2
+		in_partial_force_valid <= 1'b1;
+		particle_address <= 9'd5;
+		in_partial_force <= {32'h4048F5C3, 32'h4048F5C3, 32'h4048F5C3};				// input value: {3.14,3.14,3.14}
+		
+		// Invalidate input to let the accumulation finish
+		#2
+		in_partial_force_valid <= 1'b0;
+		
+		// Readout the final result
+		#200
+		in_read_data_request <= 1'b1;
+		in_cache_read_address <= 9'd1;
+		
+		#2
+		in_cache_read_address <= 9'd2;
+		
+		#2
+		in_cache_read_address <= 9'd3;
+		
+		#2
+		in_cache_read_address <= 9'd4;
+		
+		#2
+		in_cache_read_address <= 9'd5;
+		
+		#2
+		in_read_data_request <= 1'b0;
+		
+		//////////////////////////////////////////////////////////
+		// Final value:
+		//	1: 5.0 (32'h40A00000)
+		//	2: 2.0 (32'h40000000)
+		//	3: 4.0+1.36*2 = 6.72 (32'h40D70A3D)
+		//	4: 5.0+2.48*2 = 9.96 (32'h411F5C29)
+		//	5: 1.0+3.14*2 = 7.28 (32'h40E8F5C3)
+		//////////////////////////////////////////////////////////
+*/		
 	end
 	
 	
