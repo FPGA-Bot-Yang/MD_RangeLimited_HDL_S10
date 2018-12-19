d11de14972b1c273b0652f4417576a79208b3bb0
Ethan <ethanyang4623@gmail.com>
2018-12-19 15:39:25 -0500

    Update run_emulation.sh

--- OpenCL_HDL_Lib/test/run_emulation.sh
+++ OpenCL_HDL_Lib/test/run_emulation.sh
@@ -2,12 +2,16 @@
 cd ..
 source gen_library.sh
 cd test
+echo "Library generated!\n"
 
 # Compile host
 make
+echo "Host file compiled!\n"
 
 # Compile for emulation
 aoc -march=emulator -l ../RL_LJ_Evaluation.aoclib -L .. device/LJ.cl -o bin/LJ.aocx
+echo "Emulation file compiled!\n"
 
 # Run emulation
 env CL_CONTEXT_EMULATOR_DEVICE_INTELFPGA=1 ./bin/host
+echo "Emulation done!\n"
